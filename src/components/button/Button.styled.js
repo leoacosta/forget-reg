baaf6000decc78369c7d250cc2a97c38664bf7ab
leoacosta <hello@iamleoacosta.com>
2021-04-16 21:51:38 +1000

    update button styles

--- src/components/button/Button.styled.js
+++ src/components/button/Button.styled.js
@@ -2,16 +2,40 @@ import styled from 'styled-components';
 
 export const StyledButtonGroup = styled.div`
   margin: 2rem 0;
+  background-color: #173146;
+  border-radius: 1.875rem;
 `;
 
 export const StyledButton = styled.button`
   background-color: #173146;
   border: none;
-  border-radius: 2rem;
+  border-radius: 1.875rem;
   color: #fff;
   cursor: pointer;
   font-size: 1.25rem;
   padding: 1rem 2rem;
+
+  &:nth-child(2) {
+    position: relative;
+
+    &:before,
+    &:after {
+      content: '';
+      width: 1px;
+      height: 50%;
+      background-color: hsl(60deg 4% 89% / 25%);
+      position: absolute;
+      top: 25%;
+    }
+
+    &:before {
+      left: -5px;
+    }
+
+    &:after {
+      right: -5px;
+    }
+  }
 
   &:focus {
     outline: 0; // TODO fix for accessibility
@@ -19,5 +43,7 @@ export const StyledButton = styled.button`
 
   &.is-active {
     background-color: #4baef7;
+    position: relative;
+    z-index: 1;
   }
 `;

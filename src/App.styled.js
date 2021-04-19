9e428aec9f153dfdb673ce136a799922b6c6c52a
leoacosta <hello@iamleoacosta.com>
2021-04-19 17:21:03 +1000

    revert change

--- src/App.styled.js
+++ src/App.styled.js
@@ -3,7 +3,7 @@ import { BREAKPOINT, COLOR } from './utils/constants';
 
 export const StyledApp = styled.main`
   height: 100vh;
-  padding: 1.5rem;
+  padding: 2rem 1.5rem;
   display: grid;
   justify-items: center;
   align-items: center;
